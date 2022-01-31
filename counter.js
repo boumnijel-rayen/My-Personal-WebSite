7da5b6dc2a039848c0540257c8b9915325f80372
boumnijel-rayen <boumnijelrayen9@gmail.com>
2022-01-31 16:34:16 +0100

    a propos

---
+++ counter.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target');
+        const c = +counter.innerText;
+
+        const increament = target/400;
+
+        
+
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increament)}`;
+            setTimeout(updateCounter, 6);
+        }
+    };
+    updateCounter();
+});
+
